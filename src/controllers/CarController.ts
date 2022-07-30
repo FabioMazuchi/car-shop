eb3c3e19dce3762a894fc099e72b6ae1293c7a0b
Fabio Augusto Mazuchi <mazuchi.augusto@hotmail.com>
2022-07-30 07:59:21 -0300

    requisito 04

---
+++ src/controllers/CarController.ts
@@ -0,0 +1,17 @@
+import { Request, Response } from 'express';
+import { ICar } from '../interfaces/ICar';
+import { IService } from '../interfaces/IService';
+
+class CarController {
+  constructor(private _service: IService<ICar>) {}
+
+  async create(req: Request & { body: ICar }, res: Response<ICar>) {
+    const { model, year, color, buyValue, seatsQty, doorsQty } = req.body;
+    const car = { model, year, color, buyValue, seatsQty, doorsQty };
+    const result = await this._service.create(car);
+
+    return res.status(201).json(result);
+  }
+}
+
+export default CarController;
