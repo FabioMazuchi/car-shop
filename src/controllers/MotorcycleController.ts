40cea0f7861985d5fb3066b0b9f9326d5305a50e
Fabio Augusto Mazuchi <mazuchi.augusto@hotmail.com>
2022-07-31 20:38:04 -0300

    requisito 20

--- src/controllers/MotorcycleController.ts
+++ src/controllers/MotorcycleController.ts
@@ -23,18 +23,16 @@ class MotorcycleController {
   //   return res.status(200).json(car);
   // }
 
-  // async read(req: Request, res: Response<ICar[]>) {
-  //   const result = await this._service.read();
+  async read(req: Request, res: Response<IMotorcycle[]>) {
+    const result = await this._service.read();
 
-  //   return res.status(200).json(result);
-  // }
+    return res.status(200).json(result);
+  }
 
   async create(
     req: Request & { body: IMotorcycle },
     res: Response<IMotorcycle>,
   ) {
-    // const { model, year, color, buyValue, seatsQty, doorsQty } = req.body;
-    // const car = { model, year, color, buyValue, seatsQty, doorsQty };
     const result = await this._service.create(req.body);
 
     return res.status(201).json(result);
